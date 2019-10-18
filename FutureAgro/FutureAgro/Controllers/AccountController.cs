47fd221248f5f7fe2082a16713144474ab0c0e6d
Fabio Tangarife <fabio.tangarife@newmarketinc.com>
2019-10-18 11:31:03 -0500

    Se configura el login de usuario persistente

--- FutureAgro/FutureAgro/Controllers/AccountController.cs
+++ FutureAgro/FutureAgro/Controllers/AccountController.cs
@@ -40,7 +40,7 @@ namespace FutureAgro.Web.Controllers
         {
             // Sign in the user with this external login provider if the user already 
             // has a login.
-            var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, lockoutOnFailure: true);
+            var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: true);
 
             if (result.Succeeded)
             {
@@ -100,7 +100,7 @@ namespace FutureAgro.Web.Controllers
                     //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    await signInManager.SignInAsync(user, isPersistent: false);
+                    await signInManager.SignInAsync(user, isPersistent: true);
                     return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
